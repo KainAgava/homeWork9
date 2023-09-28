fbcd0cb5cae570853e1d5fd4bef48ebc443fa802
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-09-28 22:36:40 +0700

    Add files via upload

---
+++ homeWork2.3/homeWork2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j = 7;
+            int max = 100;
+            //for т.к. известные "рамки" цикла
+            for (i = 5; i < max; i += j)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
